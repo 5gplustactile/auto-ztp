49949c06c7adf516abe733071d0eb61af195d152
Andrés Anaya <alvaroandres.anayaamariles@telefonica.com>
2024-02-22 15:55:52 +0100

    private_ip

--- tf-modules/ec2-instance/vars.tf
+++ tf-modules/ec2-instance/vars.tf
@@ -1,13 +1,11 @@
 variable "instances" {
   description = "Map of ec2 instances"
   type = map(object({
-    private_ip = string
     instance_in_edge = bool
     ami = string
   }))
   default = {
     "instance-example" = {
-      private_ip = "172.0.5.10"
       instance_in_edge = true
       ami = "ami-05b5a865c3579bbc4"
     }
