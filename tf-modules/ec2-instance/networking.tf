547fe5f5b479d688f2e8a10f0bf25234453943da
Andrés Anaya <alvaroandres.anayaamariles@telefonica.com>
2024-02-22 15:23:07 +0100

    vpc_id

--- tf-modules/ec2-instance/networking.tf
+++ tf-modules/ec2-instance/networking.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "tf_outpost_subnet_edge" {
   count = local.instance_in_edge ? 1 : 0
-  vpc_id     = module.vpc.vpc_id
+  vpc_id     = var.vpc_id
   cidr_block = var.cidr_block_snet_op_region
   outpost_arn = local.outpost_arn
   availability_zone = "eu-west-3a"
