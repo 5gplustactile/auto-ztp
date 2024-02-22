b1728f138b034ca977b82a7f7ffa023c57916700
Andrés Anaya <alvaroandres.anayaamariles@telefonica.com>
2024-02-22 17:15:55 +0100

    eni_lni

--- tf-modules/ec2-instance/networking.tf
+++ tf-modules/ec2-instance/networking.tf
@@ -13,30 +13,18 @@ resource "aws_subnet" "tf_outpost_subnet_edge" {
   tags = var.tags
 }
 
-resource "aws_subnet" "tf_outpost_subnet_edge_local" {
-  for_each = { for k, v in var.instances : k => v if v.instance_in_edge }
-
-  vpc_id     = var.vpc_id
-  cidr_block = var.cidr_block_snet_op_region
-  outpost_arn = local.outpost_arn
-  availability_zone = "eu-west-3a"
-  enable_lni_at_device_index = 1
-
-  tags = var.tags
-}
-
 resource "aws_network_interface" "eni_lni" {
-  for_each = { for k, v in var.instances : k => v if v.instance_in_edge }
+  count = length(local.edge_instances)
 
-  subnet_id = aws_subnet.tf_outpost_subnet_edge_local[each.key].id
+  subnet_id = aws_subnet.tf_outpost_subnet_edge.id
   tags = var.tags
 }
 
 resource "aws_network_interface_attachment" "attach_eni_lni" {
-  for_each = { for k, v in var.instances : k => v if v.instance_in_edge }
+  count = length(local.instance_in_edge_ids)
 
-  instance_id   = each.key
-  network_interface_id = aws_network_interface.eni_lni[each.key].id
+  instance_id   = values(local.instance_in_edge_ids)[count.index]
+  network_interface_id = aws_network_interface.eni_lni[count.index].id
   device_index  = 1
 }
 
