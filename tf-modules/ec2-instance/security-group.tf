6b8fb96ab96ac3a66b4dbd55fe5cae601b24dba0
Andrés Anaya <alvaroandres.anayaamariles@telefonica.com>
2024-02-23 09:31:31 +0100

    cidr_blocks

--- tf-modules/ec2-instance/security-group.tf
+++ tf-modules/ec2-instance/security-group.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "sgs_instance" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = [data.aws_vpc.vpc_dc.cidr_block]
+    cidr_blocks      = [var.cidr_block_snet_op_region]
   }
   
   egress {
