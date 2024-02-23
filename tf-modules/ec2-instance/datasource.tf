8f1d41b1bcfe4529d2e10fd76e9a293c4782bce5
Andrés Anaya <alvaroandres.anayaamariles@telefonica.com>
2024-02-23 09:47:05 +0100

    data.aws_vpc.vpc_dc.cidr_block

--- tf-modules/ec2-instance/datasource.tf
+++ tf-modules/ec2-instance/datasource.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 data "aws_availability_zones" "available" {}
-#data "aws_vpc" "vpc_dc" {
-#  id = var.vpc_id
-#}
+data "aws_vpc" "vpc_dc" {
+  id = var.vpc_id
+}
