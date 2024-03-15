bf63c7dd391411d9effe0467b7b95e0666611768
andresanaya21 <alvaroandres.anayaamariles@telefonica.com>
2024-03-15 14:48:36 +0100

    helm script

--- templates/scripts/helm-template.sh
+++ templates/scripts/helm-template.sh
@@ -2,13 +2,13 @@
 # Initialize counter
 num_files=0
 # Parse command-line options
-while getopts d:z:dt: option
+while getopts d:e:t: option
 do
     case "${option}"
     in
         d) dir=${OPTARG};;
-        z) zone=${OPTARG};;
-        dt) dt=${OPTARG};;
+        e) zone=${OPTARG};;
+        t) dt=${OPTARG};;
     esac
 done
 # Print the paths of the files and the cluster name if a YAML file exists
@@ -18,7 +18,7 @@ do
     if [ -f "$file" ]; then
         cluster_name=$(yq e '.clusters[0].name' "$file")
         echo "The cluster name in the YAML file is: $cluster_name"
-        helm template -n $cluster_name auto-ztp/templates/helm-$zone-chart -f $file > auto-ztp/sites/$dt/$zone/$cluster_name.yaml 2> /dev/null
+        helm template -n $cluster_name templates/helm-$zone-chart -f $file > sites/$dt/$zone/$cluster_name.yaml 2> /dev/null
     fi
     ((num_files++))
 done
