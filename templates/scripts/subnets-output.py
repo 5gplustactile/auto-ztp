6a51b169eea893923eea73184e737fc09c3cafc6
andresanaya21 <alvaroandres.anayaamariles@telefonica.com>
2024-03-15 08:35:12 +0100

    subents-output python

--- templates/scripts/subnets-output.py
+++ templates/scripts/subnets-output.py
@@ -14,6 +14,8 @@ parser.add_argument('--path-dir', required=True, help='Path to directory with YA
 parser.add_argument('--path-dest', required=True, help='Path to destination directory')
 parser.add_argument('--git-token', required=True, help='GitHub token')
 parser.add_argument('--git-src', required=True, help='Local path to clone the GitHub repository')
+parser.add_argument('--user-name', required=True, help='User name for git config')
+parser.add_argument('--user-email', required=True, help='User email for git config')
 args = parser.parse_args()
 
 # Create a new directory for the cloned repository
@@ -47,6 +49,10 @@ else:
     # Clone the GitHub repository
     subprocess.run(['git', 'clone', f'https://{args.git_token}@github.com/5gplustactile/auto-ztp.git', git_clone_dir])
 
+    # Set the local user.name and user.email for the repository
+    subprocess.run(['git', '-C', git_clone_dir, 'config', '--local', 'user.name', args.user_name])
+    subprocess.run(['git', '-C', git_clone_dir, 'config', '--local', 'user.email', args.user_email])
+
     # Iterate over all YAML files in the specified directory
     for yaml_file_path in yaml_files:
         # Check if the file exists
@@ -54,7 +60,6 @@ else:
             # Read the YAML file
             with open(yaml_file_path, 'r') as f:
                 values = yaml.safe_load(f)
-
             # Check if 'clusters' is a list
             if isinstance(values.get('clusters'), list):
                 # Update the YAML file
@@ -67,20 +72,16 @@ else:
                             print(f"Cluster name '{cluster['name']}' in file {yaml_file_path} does not match any keys in the subnet JSON files. No changes were made to this cluster.")
                     else:
                         print(f"No 'name' field found in cluster in file {yaml_file_path}. No changes were made to this cluster.")
-
                 # Write the updated values back to a new YAML file in the destination directory
                 dest_file_path = os.path.join(args.path_dest, os.path.basename(yaml_file_path))
                 with open(dest_file_path, 'w') as f:
                     yaml.safe_dump(values, f)
-
                 # Add the file to the git staging area
                 subprocess.run(['git', '-C', git_clone_dir, 'add', dest_file_path])
-
             else:
                 print(f"'clusters' is not a list in file {yaml_file_path}. No changes were made.")
         else:
             print(f"File {yaml_file_path} does not exist. No changes were made.")
-
     # Commit the changes
     subprocess.run(['git', '-C', git_clone_dir, 'commit', '-m', 'Update YAML files'])
 
